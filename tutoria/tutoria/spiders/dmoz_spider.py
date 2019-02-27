917af076a529dc9ba0971252a6641822f94642cf
fdl66 <fan0816fan@163.com>
2019-02-28 00:50:28 +0800

    [FEATURE] 成功爬去z.i.r.u页面数据

--- tutoria/tutoria/spiders/dmoz_spider.py
+++ tutoria/tutoria/spiders/dmoz_spider.py
@@ -1,9 +1,10 @@
 # coding=utf-8
 import time
 import scrapy
-#引入容器
+# 引入容器
 from tutoria.items import CourseItem
 from tutoria.items import HouseDetail
+
 
 # class MySpider(scrapy.Spider):
 #     #设置name
@@ -33,12 +34,21 @@ from tutoria.items import HouseDetail
 #             yield item
 
 
+def _gen_url():
+    ziru_url_list = list()
+    base_url = "http://www.ziroom.com/z/nl/z2.html?qwd=%E9%A1%BA%E4%B9%89&p={}"
+    for i in range(1, 31):
+        ziru_url_list.append(base_url.format(i))
+
+    return ziru_url_list
+
+
 class Ziru(scrapy.Spider):
     name = "ziru"
     allowed_domains = ["ziroom.com"]
-    start_urls = ["http://www.ziroom.com/z/nl/z2.html?qwd=%E9%A1%BA%E4%B9%89"]
+    start_urls = _gen_url()
     custom_settings = {
-        'LOG_LEVEL': 'DEBUG',
+        # 'LOG_LEVEL': 'DEBUG',
         # 'LOG_FILE': '5688_log_{}.txt'.format(time.time()),
         "DEFAULT_REQUEST_HEADERS": {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
@@ -46,9 +56,24 @@ class Ziru(scrapy.Spider):
     }
 
     def parse(self, response):
+        """
+        //*[@id="houseList"]/li[1]/div[2]/h3/a
+        //*[@id="houseList"]/li[1]/div[3]/p[1]
+        //*[@id="houseList"]/li[1]/div[3]/p[1]/span[1]
+        //*[@id="houseList"]/li[1]/div[2]/div/p[1]/span[1]
+        //*[@id="houseList"]/li[1]/div[2]/div/p[1]/span[2]
+        //*[@id="houseList"]/li[1]/div[2]/div/p[2]/span
+        :param response:
+        :return:
+        """
         item = HouseDetail()
-        print(response.text,"++++++++++++++++++__________________++++++++++++")
-        import pdb; pdb.set_trace()
         for box in response.xpath('//*[@id="houseList"]/li[@class="clearfix"]'):
-            item['title'] = box
+            item['title'] = box.xpath(".//div[2]/h3/a/text()").extract()[0]
+            item['pic_url'] = box.xpath(".//div[1]/a/img/@src").extract()[0]
+            item['detail'] = {
+                'size': box.xpath('.//div[2]/div/p[1]/span[1]/text()').extract()[0],
+                'floor': box.xpath('.//div[2]/div/p[1]/span[2]/text()').extract()[0],
+                'house_type': box.xpath('.//div[2]/div/p[1]/span[3]/text()').extract()[0],
+                'addr': box.xpath('.//div[2]/div/p[2]/span/text()').extract()[0],
+            }
             yield item
